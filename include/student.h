c916137f434dfc8a146d8d7a708db9a495464a54
Yaseen Rather <yaseenbinashraf469@gmail.com>
2025-05-30 12:36:51 +0530

    Librarian added

--- include/student.h
+++ include/student.h
@@ -1,8 +1,27 @@
-//
-// Created by Asus on 5/29/2025.
-//
-
 #ifndef STUDENT_H
 #define STUDENT_H
+#include <string>
+#include "books.h>
+
+class IssuedBooks {
+     private:
+          Books* issuedHead;
+          int date;
+
+class student {
+     private:
+          int studentID;
+          std::string studentName;
+          books* issuedBooksHead;
+          static nextID;
+
+     public:
+          //Constructor;
+          student(std::string name);
+
+          //getters;
+          int getStudentID(); const;
+          std::string getStudentName() const;
+
 
 #endif //STUDENT_H
